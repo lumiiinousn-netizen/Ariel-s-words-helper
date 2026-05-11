d82eb4aba3b9fa9ba8001c10b7cca3d7a52eec8a
再也不熬夜了 <lumiiinousn@gmail.com>
2026-05-11 09:34:58 +0800

    Delete js directory

--- js/app.js
+++
@@ -1,3 +0,0 @@
-export const AppState = {
-    // 暂时为空，后续逐步添加
-};
--- js/modules/utils.js
+++
@@ -1,3 +0,0 @@
-export function escapeHtml(str) {
-    return String(str).replace(/[&<>]/g, m => m === '&' ? '&amp;' : m === '<' ? '&lt;' : '&gt;');
-}
